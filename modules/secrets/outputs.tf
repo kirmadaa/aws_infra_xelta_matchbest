9b37025916eea3f28a5bb04e8db2cffebeff0bfc
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-10 00:12:59 +0530

    Update outputs.tf

--- modules/secrets/outputs.tf
+++ modules/secrets/outputs.tf
@@ -8,12 +8,3 @@ output "db_secret_name" {
   value       = aws_secretsmanager_secret.db_credentials.name
 }
 
-output "app_secret_arn" {
-  description = "ARN of application secrets"
-  value       = aws_secretsmanager_secret.app_secrets.arn
-}
-
-output "app_secret_name" {
-  description = "Name of application secrets"
-  value       = aws_secretsmanager_secret.app_secrets.name
-}
