7c27e260b387ca1c7069b3e502147c013a4b52c5
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-13 16:53:09 +0530

    Delete modules/rds directory

--- modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "primary_endpoint" {
-  description = "The endpoint of the primary RDS instance"
-  value       = aws_db_instance.main[0].endpoint
-}
-
-output "read_replica_endpoints" {
-  description = "A list of endpoints for the read replicas"
-  value       = aws_db_instance.read_replica.*.endpoint
-}
