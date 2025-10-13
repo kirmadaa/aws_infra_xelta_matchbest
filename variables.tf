4637831e74e408460ed42014d997b723f879ed77
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-13 16:56:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -52,17 +52,6 @@ variable "frontend_image" {
   type        = string
 }
 
-variable "db_username" {
-  description = "Username for the RDS database"
-  type        = string
-  sensitive   = true
-}
-
-variable "db_password" {
-  description = "Password for the RDS database"
-  type        = string
-  sensitive   = true
-}
 
 variable "enable_redis" {
   description = "Enable ElastiCache Redis deployment"
