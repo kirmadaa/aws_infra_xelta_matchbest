d3f2f7f2f09fc70c1416f84b35cc08210f24d448
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-15 04:30:36 +0530

    Update outputs.tf

--- modules/ecs_service/outputs.tf
+++ modules/ecs_service/outputs.tf
@@ -1,29 +1,31 @@
+# modules/ecs_service/outputs.tf
+
+output "frontend_nlb_listener_arn" {
+  description = "ARN of the NLB listener for the frontend"
+  value       = aws_lb_listener.frontend.arn
+}
+
+output "backend_nlb_listener_arn" {
+  description = "ARN of the NLB listener for the backend"
+  value       = aws_lb_listener.backend.arn
+}
+
 output "service_security_group_id" {
   description = "ID of the security group for the ECS service"
   value       = aws_security_group.ecs_service.id
 }
 
-output "backend_ecs_service_name" {
+output "backend_service_name" {
   description = "Name of the backend ECS service"
   value       = aws_ecs_service.backend.name
 }
 
-output "frontend_ecs_service_name" {
+output "frontend_service_name" {
   description = "Name of the frontend ECS service"
   value       = aws_ecs_service.frontend.name
 }
 
-output "alb_arn_suffix" {
-  description = "ARN suffix of the Application Load Balancer"
-  value       = aws_lb.alb.arn_suffix
-}
-
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.alb.dns_name
-}
-
-output "alb_zone_id" {
-  description = "Zone ID of the Application Load Balancer"
-  value       = aws_lb.alb.zone_id
+output "nlb_arn_suffix" {
+  description = "ARN suffix of the Network Load Balancer"
+  value       = aws_lb.nlb.arn_suffix
 }
