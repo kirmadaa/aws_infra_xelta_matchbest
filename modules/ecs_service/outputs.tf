24b0cf8435e05a9bdfd8cdca9078f25b74aa3ed5
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-15 04:48:19 +0530

    Update outputs.tf

--- modules/ecs_service/outputs.tf
+++ modules/ecs_service/outputs.tf
@@ -12,3 +12,18 @@ output "service_security_group_id" {
   description = "ID of the security group for the ECS service"
   value       = aws_security_group.ecs_service.id
 }
+
+output "backend_service_name" {
+  description = "Name of the backend ECS service"
+  value       = aws_ecs_service.backend.name
+}
+
+output "frontend_service_name" {
+  description = "Name of the frontend ECS service"
+  value       = aws_ecs_service.frontend.name
+}
+
+output "nlb_arn_suffix" {
+  description = "ARN suffix of the Network Load Balancer"
+  value       = aws_lb.nlb.arn_suffix
+}
