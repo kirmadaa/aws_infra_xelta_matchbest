09c404f68abf30f6112cff83bacd85df44cdc81d
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-19 00:24:38 +0530

    Update outputs.tf

--- modules/ecs_service/outputs.tf
+++ modules/ecs_service/outputs.tf
@@ -1,19 +1,19 @@
-output "service_arn" {
-  description = "ARN of the ECS service"
-  value       = aws_ecs_service.main.id
+output "frontend_nlb_listener_arn" {
+  description = "ARN of the NLB listener for the frontend"
+  value       = aws_lb_listener.frontend.arn
 }
 
-output "service_security_group_id" {
-  description = "ID of the security group for the ECS service"
-  value       = aws_security_group.ecs_service.id
-}
-
-output "nlb_listener_arn" {
-  description = "ARN of the NLB listener"
-  value       = aws_lb_listener.main.arn
+output "backend_nlb_listener_arn" {
+  description = "ARN of the NLB listener for the backend"
+  value       = aws_lb_listener.backend.arn
 }
 
 output "nlb_arn" {
   description = "ARN of the NLB"
   value       = aws_lb.nlb.arn
+}
+
+output "service_security_group_id" {
+  description = "ID of the security group for the ECS service"
+  value       = aws_security_group.ecs_service.id
 }
