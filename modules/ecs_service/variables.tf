e3334cdd22a321bc907db073b2c77b7db0d076e3
kirmadaa <100154262+kirmadaa@users.noreply.github.com>
2025-10-29 03:21:57 +0530

    feat: add cost optimization variables to ECS service module

--- modules/ecs_service/variables.tf
+++ modules/ecs_service/variables.tf
@@ -23,6 +23,11 @@ variable "private_subnet_ids" {
   type        = list(string)
 }
 
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for the ALB"
+  type        = list(string)
+}
+
 variable "frontend_image" {
   description = "Docker image for the frontend service"
   type        = string
@@ -37,8 +42,82 @@ variable "http_api_vpclink_sg_id" {
   description = "The ID of the security group for the HTTP API Gateway VPC Link"
   type        = string
 }
-# Add this new variable
-variable "public_subnet_ids" {
-  description = "List of public subnet IDs for the ALB"
-  type        = list(string)
+
+# Cost Optimization Variables
+variable "task_cpu" {
+  description = "CPU units for ECS tasks (256, 512, 1024, 2048, 4096)"
+  type        = number
+  default     = 256
+}
+
+variable "task_memory" {
+  description = "Memory for ECS tasks in MB (512, 1024, 2048, 4096, 8192)"
+  type        = number
+  default     = 512
+}
+
+variable "desired_count" {
+  description = "Desired number of ECS tasks"
+  type        = number
+  default     = 2
+}
+
+variable "max_capacity" {
+  description = "Maximum number of ECS tasks for auto scaling"
+  type        = number
+  default     = 10
+}
+
+variable "min_capacity" {
+  description = "Minimum number of ECS tasks for auto scaling"
+  type        = number
+  default     = 1
+}
+
+variable "enable_spot_capacity" {
+  description = "Enable Fargate Spot capacity for cost optimization"
+  type        = bool
+  default     = false
+}
+
+variable "spot_capacity_percentage" {
+  description = "Percentage of capacity to run on Spot instances"
+  type        = number
+  default     = 50
+}
+
+variable "log_retention_days" {
+  description = "CloudWatch log retention in days"
+  type        = number
+  default     = 7
+}
+
+variable "enable_detailed_monitoring" {
+  description = "Enable detailed CloudWatch monitoring"
+  type        = bool
+  default     = false
+}
+
+variable "cpu_target_utilization" {
+  description = "Target CPU utilization for auto scaling"
+  type        = number
+  default     = 70
+}
+
+variable "memory_target_utilization" {
+  description = "Target memory utilization for auto scaling"
+  type        = number
+  default     = 80
+}
+
+variable "enable_service_discovery" {
+  description = "Enable ECS Service Discovery"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
 }
